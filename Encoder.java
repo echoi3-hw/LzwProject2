a0ef565770b12554a0cafb9ac928708f210dfa07
echoi3-hw <70167495+echoi3-hw@users.noreply.github.com>
2020-09-24 10:36:16 -0700

    Update Encoder.java

--- Encoder.java
+++ Encoder.java
@@ -2,9 +2,18 @@ import java.util.*;
 import java.io.*;
 
 public class Encoder {
+	/*
+	 * Edit 1:
+	 * made arraylist into HashMap
+	 */
 	HashMap <String, Integer> map = new HashMap<String,Integer>();
 	int mapSize = 128;
+	
 	public Encoder(){
+		/*
+		 * Edit 2
+		 * input all chars in ascii table into dictionary - only had a-z before
+		 */
 		//initializes the array with 0-127 from ascii table
 		for (int i = 0; i < 128; i++) {
 			map.put("" + (char)i, i);
@@ -15,7 +24,11 @@ public class Encoder {
 		BufferedReader br = new BufferedReader(new FileReader(fileName)); 
 		PrintWriter pw = new PrintWriter (new FileWriter (new File (fileName +".lzw")));
 		String code = "";
-		
+		/*
+		 * Edit 3
+		 * changed from for loop to while loop
+		 * made the loop actually work
+		 */
 		while (br.ready()) {
 			String c = "" + (char)(br.read()); //reads in new char as variable 'c'
 			String newcode = "" + code + c; //appends c onto code
